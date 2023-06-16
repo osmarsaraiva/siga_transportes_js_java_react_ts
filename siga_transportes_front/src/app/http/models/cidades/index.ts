16b328c13a2f6209d34232d324a73457e342d4b2
Osmar Saraiva Jr <osmar.saraiva@gmail.com>
2023-06-15 21:44:33 -0300

    httpClinte2

---
+++ siga_transportes_front/src/app/http/models/cidades/index.ts
@@ -0,0 +1,6 @@
+export interface Cidade {
+    id?: number;
+	cidade?: string;
+	estado?: string;
+	outro?: string;
+}
---
+++ siga_transportes_front/src/app/http/services/cidade.service.ts
@@ -0,0 +1,12 @@
+import {  httpClient } from 'app/http'
+import { Cidade } from '../models/cidades'
+import { AxiosResponse } from 'axios'
+
+const resourceURL: string = "/api/cidades"
+
+export const useCidadeService = () => {
+    const salvar = async (cidade: Cidade) : Promise<Cidade> => {
+        const response: AxiosResponse<Cidade> = await httpClient.post<Cidade>(resourceURL, cidade )
+        return response.data;
+    }
+}
